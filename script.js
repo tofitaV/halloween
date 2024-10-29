4bf8a715b0c2d45d5886cf62b28d98f44669ca8b
Vlad <48254481+tofitaV@users.noreply.github.com>
2024-10-29 20:08:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
+// Получаем элементы для музыки и значка
 const music = document.getElementById("background-music");
-        const toggleButton = document.getElementById("toggle-music");
-        const icon = toggleButton.querySelector("i");
+const toggleButton = document.getElementById("toggle-music");
+const icon = toggleButton.querySelector("i");
 
-        music.volume = 0.05;
-        toggleButton.addEventListener("click", () => {
-            if (music.paused) {
-                music.play();
-                icon.classList.remove("fa-volume-mute");
-                icon.classList.add("fa-volume-up");
-            } else {
-                music.pause();
-                icon.classList.remove("fa-volume-up");
-                icon.classList.add("fa-volume-mute");
-            }
-        });
+// Устанавливаем начальный уровень громкости
+music.volume = 0.05;
+
+// Устанавливаем значок по умолчанию при загрузке страницы
+if (music.paused) {
+    icon.classList.remove("fa-volume-up");
+    icon.classList.add("fa-volume-mute");
+}
+
+// Обработчик для переключения музыки
+toggleButton.addEventListener("click", () => {
+    if (music.paused) {
+        music.play();
+        icon.classList.remove("fa-volume-mute");
+        icon.classList.add("fa-volume-up");
+    } else {
+        music.pause();
+        icon.classList.remove("fa-volume-up");
+        icon.classList.add("fa-volume-mute");
+    }
+});
